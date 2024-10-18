cbefb5f21e34cbfa13ede48ee8a24fa261afe44b
Romano Romano <romano.romano@gmail.com>
2024-10-18 12:16:33 +0200

    feat: add variables for tags and region

--- variables.tf
+++ variables.tf
@@ -4,8 +4,23 @@ variable "aws_access_key_id" {
   type        = string
 }
 
+variable "aws_default_tags" {
+  default = {
+    github_repository = "https://github.com/BlueRaven1975/tf-aws"
+    tfe_workspace     = "tf-aws"
+  }
+  description = "Tags to be applied to all resources created by the code"
+  type        = map(string)
+}
+
 variable "aws_secret_access_key" {
   description = "AWS secret access key"
   sensitive   = true
+  type        = string
+}
+
+variable "region" {
+  default     = "eu-central-1"
+  description = "AWS region in which the components are to be deployed"
   type        = string
 }
