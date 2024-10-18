cf32a6f2699a50d8533ccc36aa0c6446d9044826
Romano Romano <romano.romano@gmail.com>
2024-10-18 12:21:07 +0200

    feat: apply default tags

--- providers.tf
+++ providers.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   access_key = var.aws_access_key_id
+  default_tags {
+    tags = var.aws_default_tags
+  }
   region     = var.region
   secret_key = var.aws_secret_access_key
 }
