e5fba07dd1b15eb022b86f2045e07c6e55ba2877
Romano Romano <romano.romano@gmail.com>
2025-02-08 19:55:54 +0100

    change: modify security group rules

--- ec2.tf
+++ ec2.tf
@@ -65,8 +65,29 @@ module "ec2_instance_sg" {
   egress_rules        = ["all-all"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  name                = "app-server-sg"
-  use_name_prefix     = false
+
+  ingress_with_cidr_blocks = [
+
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "Go hello-world app"
+      cidr_blocks = "0.0.0.0/0"
+    },
+
+    {
+      from_port   = 8081
+      to_port     = 8081
+      protocol    = "tcp"
+      description = "Python hello-world app"
+      cidr_blocks = "0.0.0.0/0"
+    },
+
+  ]
+
+  name            = "app-server-sg"
+  use_name_prefix = false
 }
 
 module "key_pair" {
