a83cbcab6248fe98e0e78a31f36e48d806e528d9
Romano Romano <romano.romano@gmail.com>
2025-02-08 19:25:44 +0100

    feat: add terraform-sa IAM user

--- iam.tf
+++ iam.tf
@@ -15,3 +15,12 @@ module "iam_user_pulumi_sa" {
   name                          = "pulumi-sa"
   policy_arns                   = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }
+
+module "iam_user_terraform_sa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-user"
+
+  create_iam_user_login_profile = false
+  force_destroy                 = true
+  name                          = "terraform-sa"
+  policy_arns                   = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,12 @@ output "pulumi_sa_aws_secret_access_key" {
   sensitive = true
   value     = module.iam_user_pulumi_sa.iam_access_key_secret
 }
+
+output "terraform_sa_aws_access_key_id" {
+  value = module.iam_user_terraform_sa.iam_access_key_id
+}
+
+output "terraform_sa_aws_secret_access_key" {
+  sensitive = true
+  value     = module.iam_user_terraform_sa.iam_access_key_secret
+}
