7ec90d28cf05d00e23b7d3f6aac13d7aab5f6886
Romano Romano <romano.romano@gmail.com>
2025-02-28 00:04:48 +0100

    security: block public access settings at account level

--- s3.tf
+++ s3.tf
@@ -1,3 +1,13 @@
+module "s3_account_block_public_access" {
+  source = "terraform-aws-modules/s3-bucket/aws//modules/account-public-access"
+
+  account_id              = data.aws_caller_identity.current.account_id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 module "s3_bucket_config" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
