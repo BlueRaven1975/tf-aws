cd53c21358fa1e2d03dbed7e15c2e033f3aac1ef
Romano Romano <romano.romano@gmail.com>
2025-08-27 21:12:12 +0200

    revert: 40575f2b2edaf3173b794a800766201ace858a9f

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,35 @@
 output "admin_aws_access_key_id" {
-  value = module.iam_user_admin.access_key_id
+  value = module.iam_user_admin.iam_access_key_id
 }
 
 output "admin_aws_secret_access_key" {
   sensitive = true
-  value     = module.iam_user_admin.access_key_secret
+  value     = module.iam_user_admin.iam_access_key_secret
 }
 
 output "github_actions_sa_aws_access_key_id" {
-  value = module.iam_user_github_actions_sa.access_key_id
+  value = module.iam_user_github_actions_sa.iam_access_key_id
 }
 
 output "github_actions_sa_aws_secret_access_key" {
   sensitive = true
-  value     = module.iam_user_github_actions_sa.access_key_secret
+  value     = module.iam_user_github_actions_sa.iam_access_key_secret
 }
 
 output "pulumi_sa_aws_access_key_id" {
-  value = module.iam_user_pulumi_sa.access_key_id
+  value = module.iam_user_pulumi_sa.iam_access_key_id
 }
 
 output "pulumi_sa_aws_secret_access_key" {
   sensitive = true
-  value     = module.iam_user_pulumi_sa.access_key_secret
+  value     = module.iam_user_pulumi_sa.iam_access_key_secret
 }
 
 output "terraform_sa_aws_access_key_id" {
-  value = module.iam_user_terraform_sa.access_key_id
+  value = module.iam_user_terraform_sa.iam_access_key_id
 }
 
 output "terraform_sa_aws_secret_access_key" {
   sensitive = true
-  value     = module.iam_user_terraform_sa.access_key_secret
+  value     = module.iam_user_terraform_sa.iam_access_key_secret
 }
