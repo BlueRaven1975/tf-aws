3f02491f4b81d69b78eb407f058221d38babda52
Romano Romano <romano.romano@gmail.com>
2025-08-27 21:08:02 +0200

    revert: 6e7f3fe138acecedddd81221b8a01750a3c21c54

--- iam.tf
+++ iam.tf
@@ -1,43 +1,35 @@
 module "iam_user_admin" {
   source = "terraform-aws-modules/iam/aws//modules/iam-user"
 
-  create_login_profile = false
-  force_destroy        = true
-  name                 = "admin"
-  policies = {
-    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
-  }
+  create_iam_user_login_profile = false
+  force_destroy                 = true
+  name                          = "admin"
+  policy_arns                   = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }
 
 module "iam_user_github_actions_sa" {
   source = "terraform-aws-modules/iam/aws//modules/iam-user"
 
-  create_login_profile = false
-  force_destroy        = true
-  name                 = "github-actions-sa"
-  policies = {
-    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
-  }
+  create_iam_user_login_profile = false
+  force_destroy                 = true
+  name                          = "github-actions-sa"
+  policy_arns                   = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }
 
 module "iam_user_pulumi_sa" {
   source = "terraform-aws-modules/iam/aws//modules/iam-user"
 
-  create_login_profile = false
-  force_destroy        = true
-  name                 = "pulumi-sa"
-  policies = {
-    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
-  }
+  create_iam_user_login_profile = false
+  force_destroy                 = true
+  name                          = "pulumi-sa"
+  policy_arns                   = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }
 
 module "iam_user_terraform_sa" {
   source = "terraform-aws-modules/iam/aws//modules/iam-user"
 
-  create_login_profile = false
-  force_destroy        = true
-  name                 = "terraform-sa"
-  policies = {
-    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
-  }
+  create_iam_user_login_profile = false
+  force_destroy                 = true
+  name                          = "terraform-sa"
+  policy_arns                   = ["arn:aws:iam::aws:policy/AdministratorAccess"]
 }
